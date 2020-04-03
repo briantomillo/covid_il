d323ee95ec12da6a0d3999f6d2e2d574d082f5a9
Brian Tomillo <brian.tomillo@gmail.com>
2020-04-03 11:04:26 -0500

    Moved dataset to /data. Additional console logging.

--- data.js
+++ data.js
@@ -9,11 +9,14 @@ const url='http://www.dph.illinois.gov/sitefiles/COVIDTestResults.json?nocache=1
 getStateData(url);
 
 function getStateData(url) {
+  console.log('Retrieving data...')
   request(url, function (error, response, body) {
     if (error) {
+      console.error('Retrieval failed with an error...')
       console.error('error:', error);
     };
     if (response.statusCode == 200) {
+      console.log('Data retrieved');
       let updateDate = JSON.parse(body)
       let updateDay = updateDate.LastUpdateDate.day;
       let updateMonth = updateDate.LastUpdateDate.month;
@@ -22,7 +25,7 @@ function getStateData(url) {
       // Add date check, ignore file unless newer
 
       datestamp = ('0' + updateMonth).slice(-2) + ('0' + updateDay).slice(-2) + updateYear
-      filename = 'dataset-' + datestamp + '.json'
+      filename = './data/dataset-' + datestamp + '.json'
       console.log('filename = ' + filename)
 
       fs.writeFile(filename, body, (error) => {
@@ -30,6 +33,7 @@ function getStateData(url) {
         console.log('Data written to file');
       });
     } else {
+      console.log('Retrieval failed with a non-200 status')
       console.log('statusCode:', response && response.statusCode);
     };
   });
